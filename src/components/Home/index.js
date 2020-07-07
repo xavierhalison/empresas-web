0286445ee853e9637d6039241ccfff11a3be6404
xavierhalison <halisonpx@gmail.com>
2020-07-07 19:46:56 -0300

    Creating first Home component screen

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -1,7 +1,26 @@
 import React from "react";
+import logo from "../../assets/logo-nav.png";
+import searchIcon from "../../assets/ic-search-copy.png";
+import "./style.css";
 
 export default class Home extends React.Component {
   render() {
-    return <h1>Home</h1>;
+    return (
+      <div className="home">
+        <div className="navbar">
+          <img className="navbar__logo" src={logo} alt="Logo da ioasys" />
+          <img
+            className="navbar__search-icon"
+            src={searchIcon}
+            alt="pesquisar"
+          />
+        </div>
+        <div className="content">
+          <span className="content__empty-state">
+            Clique na busca para iniciar
+          </span>
+        </div>
+      </div>
+    );
   }
 }
